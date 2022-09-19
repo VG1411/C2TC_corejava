3377a3ae2dad9263a413ead26609ea1bc8d03138
Arpitha <Arpitha@DESKTOP-Q35L43B>
2022-09-19 11:32:56 +0530

    First commit

---
+++ B25_corejava/src/com/tns/assignment2/This_keyword.java
@@ -0,0 +1,17 @@
+package com.tns.assignment2;
+
+public class This_keyword {
+	int a;
+	public This_keyword(int a)
+	{
+		this.a = a;
+	}
+
+	public static void main(String[] args) {
+		This_keyword ob = new This_keyword(6);
+		System.out.println("Value of a ="+ ob.a);
+		
+
+	}
+
+}
---
+++ B25_corejava/src/com/tns/assignment2/stud.java
@@ -0,0 +1,13 @@
+package com.tns.assignment2;
+
+public class stud {
+	stud()
+	{
+		System.out.println("Student object is created");
+	}
+
+	public static void main(String[] args) {
+		stud obj = new stud();
+		}
+
+}
