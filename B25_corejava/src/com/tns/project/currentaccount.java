8d8a415541de31bac6c52262e1120b4d12f55cda
Arpitha <Arpitha@DESKTOP-Q35L43B>
2022-11-02 11:40:36 +0530

    First commit

---
+++ B25_corejava/src/com/tns/project/currentaccount.java
@@ -0,0 +1,13 @@
+package com.tns.project;
+
+public abstract class currentaccount extends BankAccount  {
+	 
+		public currentaccount(int accNo, String accName, float accBal) {
+			super(accNo, accName, accBal);
+			// TODO Auto-generated constructor stub
+		}
+		private final float creditLimit = 0;
+	 
+		public abstract float withdraw();
+		public abstract String toString();
+}
